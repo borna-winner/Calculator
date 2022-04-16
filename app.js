38c4d8b4ae387ad110a5794c2c8e9b33a4e5336b
james <james@whygee.com>
2022-04-16 16:16:29 -0400

    added hover css and basic math

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-aaaaaaaa
+function add(a,b){
+    return a + b;
+}
+function subtract(a,b){
+    return a - b;
+}
+function multiply(a,b){
+    return a * b;
+}
+function divide(a,b){
+    return a / b;
+}
+//       takes the math symbol used and makes everything work together
+function operate(operator, firstArg, secondArg){
+    switch (operator){
+        case '+':
+            return add(firstArg,secondArg);
+            break;
+        case '-':
+            return subtract(firstArg,secondArg);
+            break;
+        case 'x':
+            return multiply(firstArg,secondArg);
+            break;
+        case '*':   
+            return multiply(firstArg,secondArg);
+            break;
+        case '/':
+            return divide(firstArg,secondArg);
+            break;
+        }
+    }
+
+    document.querySelectorAll('.operatorBtn').forEach(item => {
+        item.addEventListener('click', (event) => {
+                updateOperator(item.textContent);
+            })
+    }),
+// Call a function and save the return value in x:
+var x = myFunction(4, 3);
+
+function myFunction(a, b) {
+  // Return the product of a and b
+  return a * b;
+}
